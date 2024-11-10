4e9190dded5517f9f22dd9b6690ee00f24f60089
michel1991 <michelmvoumamvouma@gmail.com>
2024-11-10 19:18:57 +0100

    change return type to primitive return types

--- backend/src/mandalore-express-booking/src/mandalore-express-booking-domain/src/main/java/org/manmvou/mandaloreexpress/booking/domain/spi/IsSelectionComplete.java
+++ backend/src/mandalore-express-booking/src/mandalore-express-booking-domain/src/main/java/org/manmvou/mandaloreexpress/booking/domain/spi/IsSelectionComplete.java
@@ -1,5 +1,5 @@
 package org.manmvou.mandaloreexpress.booking.domain.spi;
 import java.util.UUID;
 public interface IsSelectionComplete {
-    public Boolean of(UUID searchId);
+    public boolean of(UUID searchId);
 }
