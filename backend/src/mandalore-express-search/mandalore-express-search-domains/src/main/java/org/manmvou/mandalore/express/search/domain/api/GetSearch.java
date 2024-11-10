11c6890b1bdacb2d78c9a87a603d3e660bb4dadf
michel1991 <michelmvoumamvouma@gmail.com>
2024-11-10 06:38:26 +0100

    add interface api to search

---
+++ backend/src/mandalore-express-search/mandalore-express-search-domains/src/main/java/org/manmvou/mandalore/express/search/domain/api/GetSearch.java
@@ -0,0 +1,20 @@
+package org.manmvou.mandalore.express.search.domain.api;
+
+import org.manmvou.mandalore.express.search.domain.Search;
+import org.manmvou.mandalore.express.search.domain.spi.Searches;
+
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+interface GetSearch {
+    Searches getSearches();
+
+    default Search identifiedBy(UUID searchId) {
+        Search foundSearch = getSearches().findSearchIdentifiedBy(searchId);
+        if (foundSearch != null) {
+            return foundSearch;
+        } else {
+            throw new NoSuchElementException("Unknown search " + searchId);
+        }
+    }
+}
