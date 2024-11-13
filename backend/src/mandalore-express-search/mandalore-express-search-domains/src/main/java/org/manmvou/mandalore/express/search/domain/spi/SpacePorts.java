110daa6b9e76054a00277fc7a0071a7d5594420a
michel1991 <michelmvoumamvouma@gmail.com>
2024-11-13 05:25:46 +0100

    add default method to find space port according to port id

--- backend/src/mandalore-express-search/mandalore-express-search-domains/src/main/java/org/manmvou/mandalore/express/search/domain/spi/SpacePorts.java
+++ backend/src/mandalore-express-search/mandalore-express-search-domains/src/main/java/org/manmvou/mandalore/express/search/domain/spi/SpacePorts.java
@@ -2,9 +2,25 @@ package org.manmvou.mandalore.express.search.domain.spi;
 
 import org.manmvou.mandalore.express.search.domain.spaceport.SpacePort;
 
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 @FunctionalInterface
 public interface SpacePorts {
     Set<SpacePort> getAllSpacePorts();
+
+    private SpacePort findBySpacePortId(String spacePortId) {
+        String id = spacePortId.contains("/") ?
+                spacePortId.substring(spacePortId.lastIndexOf("/") + 1) :
+                spacePortId;
+
+        return getAllSpacePorts().stream()
+                .filter(port -> port.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("SpacePort not found: " + id));
+    }
+
+    default SpacePort find(String spacePortId){
+        return findBySpacePortId(spacePortId);
+    }
 }
